2d1924f85b6ce5a1815621ccf56874d7a9d7b19f
huangk24 <huangk24@uw.edu>
2022-04-08 23:43:24 -0700

    week 1 assignment

---
+++ week1/index.js
@@ -0,0 +1,28 @@
+// const http = require("http");
+import http from 'http';
+// const fs = require("fs");
+import fs from 'fs';
+
+http.createServer((req,res) => {
+    var path = req.url.toLowerCase();
+    switch(path) {
+        case '/':
+            fs.readFile("home.html", (err, data) => {
+                if (err) return console.error(err);
+                   res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data.toString());
+            });
+            break;
+        case '/about':
+            fs.readFile("about.html", (err, data) => {
+                if (err) return console.error(err);
+                   res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data.toString());
+            });
+           break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not found');
+            break;
+    }
+}).listen(process.env.PORT || 3000);
