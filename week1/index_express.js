70ab92bdcc11468753af4756bf1469e357b47056
huangk24 <huangk24@uw.edu>
2022-04-25 18:41:01 -0700

    fix home and detail page

--- week1/index_express.js
+++ week1/index_express.js
@@ -14,16 +14,14 @@ app.set('view engine', 'ejs');
 // send static file as response
 app.get('/', (req,res) => {
     res.type('text/html');
-    res.render('home', {cars : [{name: "M8 Competition Gran Coupe", price: 130000}, 
-                                {name: "CLS 53 AMG", price: 87550},
-                                {name: "RS7 Sportback", price: 118500}]});
+    res.render('home', {names : cars.getAll()});
     //res.sendFile('home.html');
 });
 
 app.get('/detail', (req,res) => {
     res.type('text/html');
     let carname = req.query.name;   
-    res.render("detail", {name : JSON.stringify(carname)});
+    res.render("detail", {car : cars.getItem(carname)});
     //res.render("Detail for " + req.query.name);
 });
 
