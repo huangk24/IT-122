def2c561d71cdea3cfc9524db170f718d98ecb06
huangk24 <huangk24@uw.edu>
2022-05-16 20:49:09 -0700

    api routes

--- week1/index_express.js
+++ week1/index_express.js
@@ -1,14 +1,17 @@
 import http from 'http';
 import fs from 'fs';
-//import * as cars from './data.js';
+import * as Cars from './data.js';
 import { Car } from './Cars.js';
 import { parse } from "querystring";
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('./public')); // set location for static files
 app.use(express.urlencoded()); //Parse URL-encoded bodies
+app.use(express.json()); //Used to parse JSON bodies
+app.use('/api', cors()); // set Access-Control-Allow-Origin header for api route
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -28,6 +31,40 @@ app.get('/', (req, res, next) => {
         .catch(err => next(err));
 });
 
+app.get('/api/cars', (req,res) => {
+    const cars = Cars.getAll(); // return all cars in data store
+    if (cars) {
+      res.json(cars);
+    } else {
+      return res.status(500).send('Database Error occurred');
+    }
+  });
+
+app.get('/api/cars/:name', (req,res) => {
+  const car = Cars.getItem(req.params.name); // return a car
+  if (car) {
+    res.json(car);
+  } else {
+    return res.status(500).send('Database Error occurred');
+  }
+});
+
+app.post('/api/add', (req,res) => {
+    const newCar = {'name': req.body.name, 'brand' : req.body.brand, 'horsepower' : req.body.horsepower, 
+    'price' : req.body.price}
+    Car.updateOne({'name': req.body.name}, newCar, {upsert:true}, (err, result) => {
+        if (err) return next(err);
+        console.log(req.body.name + " Added Successfully");
+    });
+});
+
+app.get('/api/delete/:name', (req,res) => {
+    Car.deleteOne( {'name': req.params.name}, () => {
+        res.send(req.params.name + " Deleted Successfully");
+    })
+  });
+
+
 //app.get('/detail', (req,res) => {
  //   res.type('text/html');
  //   let carname = req.query.name;   
@@ -43,7 +80,7 @@ app.get('/', (req, res, next) => {
 
 app.get('/detail', (req,res,next) => {
     // db query can use request parameters
-    Car.findOne({ title:req.query.title }).lean()
+    Car.findOne({ name:req.query.name }).lean()
         .then((car) => {
             res.render('detail', {result: car} );
         })
